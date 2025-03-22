63417c63d2795cc650e84bb0818982c45a8c5731
Michal-Smolarek <smolar1202@gmail.com>
2025-03-22 19:25:47 +0100

    Added Models and Controller

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineShoppingPlatform.Models;
+
+namespace OnlineShoppingPlatform.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ShopContext _shopContext;
+        public ProductsController(ShopContext shopContext)
+        {
+            _shopContext = shopContext;
+            _shopContext.Database.EnsureCreated();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAllProducts()
+        {
+            var result = await _shopContext.Products.ToListAsync<Product>();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var result = await _shopContext.Products.FindAsync(id);
+            if(result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OnlineShoppingPlatform.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CategoryId { get; set; }
+        [JsonIgnore]
+        public virtual Category Category { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public bool IsAvaliable { get; set; }
+
+    }
+}
